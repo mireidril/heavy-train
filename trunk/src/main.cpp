bbf9f2cc80212eff0e2e98d088831958e2c7c9d7
celine.cogny <celine.cogny@f4868400-00f5-6c66-ba9b-aec0432c6665>
2012-01-21 11:23:49 +0000

    Rajout des dépendances

---
+++ trunk/src/main.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+
+#ifdef _WIN32
+#include "SDL/SDL.h"
+#include "tinyXML/tinyxml.h"
+#include "Box2D/Box2D.h"
+#else
+#include <SDL/SDL.h>
+#include <tinyXML/tinyxml.h>
+#endif
+
+int main(int argc, char** argv)
+{
+	std::cout<<"Bonjour"<<std::endl;
+
+	//SDL
+	if(SDL_Init(SDL_INIT_VIDEO) != 0) 
+	{
+		std::cerr << "Probleme pour initialiser SDL : " << SDL_GetError() << std::endl;
+	}
+
+	SDL_WM_SetCaption("Heavy Train", NULL);
+
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
+	SDL_Surface * screen = SDL_SetVideoMode(800, 600, 32, SDL_OPENGL | SDL_RESIZABLE);
+
+	//tinyXML
+	TiXmlDocument doc( "../levels/test.xml" );
+	bool loadOkay = doc.LoadFile();
+	if ( !loadOkay )
+	{
+		printf( "Could not load test file 'demotest.xml'. Error='%s'. Exiting.\n", doc.ErrorDesc() );
+	}
+
+	doc.Print();
+
+	//box2D
+	b2Vec2 gravity(0.0f, -10.0f);
+
+	// Construct a world object, which will hold and simulate the rigid bodies.
+	b2World world(gravity);
+
+	// Define the ground body.
+	b2BodyDef groundBodyDef;
+	groundBodyDef.position.Set(0.0f, -10.0f);
+
+	// Call the body factory which allocates memory for the ground body
+	// from a pool and creates the ground box shape (also from a pool).
+	// The body is also added to the world.
+	b2Body* groundBody = world.CreateBody(&groundBodyDef);
+
+	// Define the ground box shape.
+	b2PolygonShape groundBox;
+
+	// The extents are the half-widths of the box.
+	groundBox.SetAsBox(50.0f, 10.0f);
+
+	// Add the ground fixture to the ground body.
+	groundBody->CreateFixture(&groundBox, 0.0f);
+
+	// Define the dynamic body. We set its position and call the body factory.
+	b2BodyDef bodyDef;
+	bodyDef.type = b2_dynamicBody;
+	bodyDef.position.Set(0.0f, 4.0f);
+	b2Body* body = world.CreateBody(&bodyDef);
+
+	// Define another box shape for our dynamic body.
+	b2PolygonShape dynamicBox;
+	dynamicBox.SetAsBox(1.0f, 1.0f);
+
+	// Define the dynamic body fixture.
+	b2FixtureDef fixtureDef;
+	fixtureDef.shape = &dynamicBox;
+
+	// Set the box density to be non-zero, so it will be dynamic.
+	fixtureDef.density = 1.0f;
+
+	// Override the default friction.
+	fixtureDef.friction = 0.3f;
+
+	// Add the shape to the body.
+	body->CreateFixture(&fixtureDef);
+
+	// Prepare for simulation. Typically we use a time step of 1/60 of a
+	// second (60Hz) and 10 iterations. This provides a high quality simulation
+	// in most game scenarios.
+	float32 timeStep = 1.0f / 60.0f;
+	int32 velocityIterations = 6;
+	int32 positionIterations = 2;
+
+	// This is our little game loop.
+	for (int32 i = 0; i < 60; ++i)
+	{
+		// Instruct the world to perform a single step of simulation.
+		// It is generally best to keep the time step and iterations fixed.
+		world.Step(timeStep, velocityIterations, positionIterations);
+
+		// Now print the position and angle of the body.
+		b2Vec2 position = body->GetPosition();
+		float32 angle = body->GetAngle();
+
+		printf("%4.2f %4.2f %4.2f\n", position.x, position.y, angle);
+	}
+
+
+#ifdef _WIN32
+	system("PAUSE");
+#endif
+
+	return 0;
+}
