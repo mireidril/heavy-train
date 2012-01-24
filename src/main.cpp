283a6486cf30353e5a067a7d4c3f0a78c09f702c
celine.cogny <celine.cogny@f4868400-00f5-6c66-ba9b-aec0432c6665>
2012-01-24 22:28:02 +0000

    Test du moteur physique avec des images moches.

--- src/main.cpp
+++ src/main.cpp
@@ -24,8 +24,28 @@ int main(int argc, char** argv)
 
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 
-	SDL_Surface * screen = SDL_SetVideoMode(800, 600, 32, SDL_OPENGL | SDL_RESIZABLE);
+	SDL_Surface * screen = SDL_SetVideoMode(800, 600, 32, SDL_ANYFORMAT);
+	if(screen == NULL)
+	{
+		std::cout<<"erreur initialisation screen"<<std::endl;
+	}
 
+	SDL_Surface * image = SDL_LoadBMP("../img/test.bmp");
+	if(image == NULL)
+	{
+		std::cout<<"erreur chargement image"<<std::endl;
+	}
+
+	SDL_Surface * image2 = SDL_LoadBMP("../img/sol.bmp");
+	if(image2 == NULL)
+	{
+		std::cout<<"erreur chargement image2"<<std::endl;
+	}
+
+	int colorkey = SDL_MapRGB(screen->format, 255, 255, 255);
+	SDL_SetColorKey(image, SDL_SRCCOLORKEY | SDL_RLEACCEL, colorkey);
+
+	
 	//tinyXML
 	TiXmlDocument doc( "../levels/test.xml" );
 	bool loadOkay = doc.LoadFile();
@@ -37,14 +57,17 @@ int main(int argc, char** argv)
 	doc.Print();
 
 	//box2D
-	b2Vec2 gravity(0.0f, -10.0f);
+	b2Vec2 gravity(4.0f, -10.0f);
 
 	// Construct a world object, which will hold and simulate the rigid bodies.
 	b2World world(gravity);
 
 	// Define the ground body.
 	b2BodyDef groundBodyDef;
-	groundBodyDef.position.Set(0.0f, -10.0f);
+	groundBodyDef.position.Set(0.0f, -30.0f);
+	SDL_Rect * posSol = new SDL_Rect;
+	posSol->x = 0.0;
+	posSol->y = 30 + 249/2 + 20;
 
 	// Call the body factory which allocates memory for the ground body
 	// from a pool and creates the ground box shape (also from a pool).
@@ -55,7 +78,7 @@ int main(int argc, char** argv)
 	b2PolygonShape groundBox;
 
 	// The extents are the half-widths of the box.
-	groundBox.SetAsBox(50.0f, 10.0f);
+	groundBox.SetAsBox(800.0f, 10.0f);
 
 	// Add the ground fixture to the ground body.
 	groundBody->CreateFixture(&groundBox, 0.0f);
@@ -63,7 +86,9 @@ int main(int argc, char** argv)
 	// Define the dynamic body. We set its position and call the body factory.
 	b2BodyDef bodyDef;
 	bodyDef.type = b2_dynamicBody;
-	bodyDef.position.Set(0.0f, 4.0f);
+	bodyDef.position.Set(400.0f, 4.0f);
+	SDL_Rect * posSprite = new SDL_Rect;
+	posSprite->x = 400.f;
 	b2Body* body = world.CreateBody(&bodyDef);
 
 	// Define another box shape for our dynamic body.
@@ -80,6 +105,8 @@ int main(int argc, char** argv)
 	// Override the default friction.
 	fixtureDef.friction = 0.3f;
 
+	//fixtureDef.restitution = 0.8f;
+
 	// Add the shape to the body.
 	body->CreateFixture(&fixtureDef);
 
@@ -91,7 +118,7 @@ int main(int argc, char** argv)
 	int32 positionIterations = 2;
 
 	// This is our little game loop.
-	for (int32 i = 0; i < 60; ++i)
+	for (int32 i = 0; i < 1000; ++i)
 	{
 		// Instruct the world to perform a single step of simulation.
 		// It is generally best to keep the time step and iterations fixed.
@@ -102,12 +129,24 @@ int main(int argc, char** argv)
 		float32 angle = body->GetAngle();
 
 		printf("%4.2f %4.2f %4.2f\n", position.x, position.y, angle);
+
+		/* Copie de l'image à l'écran */
+		//posSprite->x = position.x +10;
+		posSprite->y = -position.y +10;
+		posSol->x = -position.x+10;
+		SDL_FillRect( SDL_GetVideoSurface(), NULL, 0 );
+		SDL_BlitSurface(image, NULL, screen, posSprite);
+		SDL_BlitSurface(image2, NULL, screen, posSol);
+	
+		/* Mise à jour de la zone d'affichage de la fenetre */
+		SDL_UpdateRect(screen, 0, 0, 0, 0);
 	}
 
 
 #ifdef _WIN32
 	system("PAUSE");
 #endif
-
+	SDL_FreeSurface(image);
+	SDL_Quit();
 	return 0;
 }
